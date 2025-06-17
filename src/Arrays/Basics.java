bd59e9a282a593e831f371c44964ddbc96a29fb4
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2025-06-17 08:58:43 +0530

    Pattern1 Complete

--- src/Arrays/Basics.java
+++ src/Arrays/Basics.java
@@ -2,6 +2,6 @@ package Arrays;
 
 public class Basics {
     public static void main(String args[]) {
-        System.out.println("Heyy");
+
     }
 }
--- src/Patterns/Pattern1.java
+++ src/Patterns/Pattern1.java
@@ -1,7 +1,21 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class Pattern1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 0;
+        while (i < n) {
+            int j = 0;
+            while (j < n) {
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
     }
 }

a18bf047616df6e0e591246488ccce508d4c0498
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2025-06-26 09:41:16 +0530

    Reverse a number

---
+++ src/Patterns/Pattern7.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row = 0;
+        int space = n - 2;
+
+    }
+}
---
+++ src/ReverseNumber.java
@@ -0,0 +1,14 @@
+public class ReverseNumber {
+
+        public static void main(String[] args) {
+            int num = 123456789;
+            int ans = 0;
+            while(num > 0){
+                int temp = num % 10;
+                ans = ans * 10 + temp;
+                num = num / 10;
+            }
+            System.out.print(ans);
+        }
+    }
+
